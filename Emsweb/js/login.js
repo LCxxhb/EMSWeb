9cc1534dd851a6105035af603e765ca06dc24d2f
Userzhangdi <16638649466@163.com>
2019-11-05 16:26:21 +0800

    修改js

--- Emsweb/js/login.js
+++ Emsweb/js/login.js
@@ -1,39 +1,40 @@
 ﻿var login = {
-    init: function () {
-        login.Login();
-    },
-    Login: function () {
-        $('#formlogin input').bind('keyup', function (event) {
-            if (event.keyCode == "13") {
-                //回车执行查询
-                $('#btnLogin').click();
-            }
-        });
-        $("#btnLogin").click(function () {
-            if (Ter.checkForm("formlogin")) {
-                var username = $("#UserName").val();
-                var pwd = $("#Password").val();
-                Ter.getApi({
-                    apiname: '/user/login',
-                    params: {
-                        username: username,
-                        password: pwd,
-                        KeyFrom: 'web'
-                    }
-                }, function (res) {               
-                    if (res.errCode != 'SUCCESS') {
-                        message = '用户名或密码不正确！';
-                        layer.alert(message, {
-                            title: '提示'
-                        });
-                    }else{
-                    	 location.href = 'index.html';
-                    }
-                });
-            }
-        })
-    }
+	init: function() {
+		login.Login();
+	},
+	Login: function() {
+		$('#formlogin input').bind('keyup', function(event) {
+			if(event.keyCode == "13") {
+				//回车执行查询
+				$('#btnLogin').click();
+			}
+		});
+		$("#btnLogin").click(function() {
+			if(Ter.checkForm("formlogin")) {
+				var username = $("#UserName").val();
+				var pwd = $("#Password").val();
+				Ter.getApi({
+					apiname: '/user/login',
+					params: {
+						username: username,
+						password: pwd,
+						KeyFrom: 'web'
+					}
+				}, function(res) {
+					if(res.errCode != 'SUCCESS') {
+						message = '用户名或密码不正确！';
+						layer.alert(message, {
+							title: '提示'
+						});
+					} else {
+						localStorage.setItem('userInfo', JSON.stringify(res.result));
+						location.href = 'index.html';
+					}
+				});
+			}
+		})
+	}
 }
-$(function () {
-    login.init();
+$(function() {
+	login.init();
 });
