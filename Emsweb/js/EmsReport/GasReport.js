7bd6e6207b062c4209712e4b4b41c70a8c075863
jfy <1712563402@qq.com>
2019-11-12 14:59:30 +0800

    报表前端页面部分数据绑定

--- Emsweb/js/EmsReport/GasReport.js
+++ Emsweb/js/EmsReport/GasReport.js
@@ -3,9 +3,9 @@ var GasReport = {
 		this.initDate(); //日期初始化
 		this.calcTableHeight();
 		this.initTable();
-		this.getFactory();
 		this.chart();
 		this.tabChange();
+		this.LoadModalAreaSelect();
 	},
 	initDate: function() {
 		//日开始
@@ -53,266 +53,199 @@ var GasReport = {
 			pageList: [5, 10, 15, 20],
 			striped: true, //隔行变色
 			columns: [{
-				field: 'factory',
+				field: 'areaname',
 				title: '分厂',
 				align: 'center'
 			}, {
-				field: 'area',
+				field: 'branchfactory',
 				title: '区域',
 				align: 'center'
 			}, {
-				field: 'point',
+				field: 'collectionpoint',
 				title: '采集点',
 				align: 'center'
 			}, {
-				field: 'describe',
+				field: 'description',
 				title: '采集点描述',
 				align: 'center'
-			},{
-				field: 'medium',
+			}, {
+				field: 'tagtype',
 				title: '介质类型',
 				align: 'center'
 			}, {
-				field: 'type',
+				field: 'usetype',
 				title: '产出/消耗',
 				align: 'center'
 			}, {
-				field: 'value',
+				field: 'tagval',
 				title: '产出/消耗量',
 				align: 'center'
 			}, {
-				field: 'time',
+				field: 'readTime',
 				title: '日期',
 				align: 'center'
 			}],
-			data: [{
-				factory: '炼铁厂',
-				area: '煤气液化炉',
-				point: '',
-				describe: '',
-				medium: '',
-				type: '',
-				value: '',
-				time: ''
-			}, {
-				factory: '炼铁厂',
-				area: '1#高炉',
-				point: '',
-				describe: '',
-				medium: '',
-				type: '',
-				value: '',
-				time: ''
-			}, {
-				factory: '炼铁厂',
-				area: '2#高炉',
-				point: '',
-				describe: '',
-				medium: '',
-				type: '',
-				value: '',
-				time: ''
-			}, {
-				factory: '炼铁厂',
-				area: '3#热风炉',
-				point: '',
-				describe: '',
-				medium: '',
-				type: '',
-				value: '',
-				time: ''
-			}, {
-				factory: '炼铁厂',
-				area: '球团',
-				point: '',
-				describe: '',
-				medium: '',
-				type: '',
-				value: '',
-				time: ''
-			}, {
-				factory: '炼铁厂',
-				area: '白灰',
-				point: '',
-				describe: '',
-				medium: '',
-				type: '',
-				value: '',
-				time: ''
-			}, {
-				factory: '炼铁厂',
-				area: '20000制氧',
-				point: '',
-				describe: '',
-				medium: '',
-				type: '',
-				value: '',
-				time: ''
-			}, {
-				factory: '炼铁厂',
-				area: '6000制氧',
-				point: '',
-				describe: '',
-				medium: '',
-				type: '',
-				value: '',
-				time: ''
-			}, {
-				factory: '炼铁厂',
-				area: '3#高炉富氧',
-				point: '',
-				describe: '',
-				medium: '',
-				type: '',
-				value: '',
-				time: ''
-			}, {
-				factory: '炼钢厂',
-				area: '炼钢氧气',
-				point: '',
-				describe: '',
-				medium: '',
-				type: '',
-				value: '',
-				time: ''
-			}, {
-				factory: '炼钢厂',
-				area: '轧钢氧气',
-				point: '',
-				describe: '',
-				medium: '',
-				type: '',
-				value: '',
-				time: ''
-			}]
+
 		});
 	},
-	getFactory: function() {
-		var factorys = ["炼铁厂", "炼钢厂", "轧钢厂", "外单位"];
-		window.onload = start;
-		var gf = document.getElementById("gasFactory");
+	//加载模态框区域以及下拉框
+	LoadModalAreaSelect: function(id) {
+		$("#gasFactory").empty();
+		Ter.getApi({
+				apiname: "/region/findByTwoRegion"
+			},
+			function(res) {
+				if(res.result) {
+					var select = $("#gasFactory");
+					select.append("<option value=''>--请选择--</option>")
+					for(var i = 0; i < res.result.length; i++) {
+						if(id == res.result[i].aid) {
+							select.append("<option value='" + res.result[i].aid + "' selected='selected'>" +
+								res.result[i].aname + "</option>");
+						} else {
+							select.append("<option value='" + res.result[i].aid + "'>" +
+								res.result[i].aname + "</option>");
+						}
+					}
+				}
+			})
+	},
+	//加载模态框区域二级下拉框
+	loadChildSlect: function() {
+		$("#gasArea").empty(); //重置下拉框
+		var aid = $.trim($('#gasFactory option:selected').val()); //获取选中的区域
+		var select = $("#gasArea");
+		select.append("<option value=''>--请选择--</option>")
+		if(aid == "") {
+			layer.alert("请先选择分厂！")
+			return;
+		}
+		Ter.getApi({
+				apiname: '/region/findByPid',
+				params: {
+					"pid": aid
+				}
+			},
+			function(res) {
+				console.log(res);
+				if(res.result) {
+					for(var i = 0; i < res.result.length; i++) {
+						select.append("<option value='" + res.result[i].aid + "'>" +
+							res.result[i].aname + "</option>");
+					}
+				}
+			})
+	},
+	//按分厂查询数据绑定
+	loadTableData: function() {
+		var url = "/report/findByAreaname";
+		var factoryName = $("#gasFactory option:checked").text();
+		Ter.getApi({
+				apiname: url,
+				params: {
+					"areaname": factoryName,
+				}
+			},
+			function(res) {
+				if(res.result) {
+					console.log(res.result);
+					//加载表格
+					$("#instantTable").bootstrapTable('load', res.result);
+				}
+			});
+		//		var areaUrl = "/report/findByBranchfactory";
+		//		var branchfactory = $("#gasArea option:checked").text();
+		//		Ter.getApi({
+		//				apiname: areaUrl,
+		//				params: {
+		//					"branchfactory": branchfactory,
+		//				}
+		//			},
+		//			function(res) {
+		//				if(res.result) {
+		//					console.log(res.result);
+		//					//加载表格
+		//					$("#instantTable").bootstrapTable('load', res.result);
+		//				}
+		//			})
+	},
+	LoadMediumTypeButton: function(id) {
+		$("#MediumType").empty();
+		var url = "/report/findByTag";
+		//		var mediumName = document.getElementById(MediumType).valueOf();
+		Ter.getApi({
+				apiname: url
+				//				params: {
+				//					"mediumName": mediumName,
+				//				}
+			},
+			function(res) {
+				if(res.result) {
 
-		function start() {
-			for(var i = 0; i < factorys.length; i++) {
-				var op = document.createElement("option");
-				op.innerHTML = factorys[i];
-				gf.appendChild(op); //添加可选择的分厂到第一个选项下拉栏
-			}
-		}
+				}
+			})
 	},
-	getArea: function() {
-		var areas = [
-			["球团", "炼铁老区", "回转窑"],
-			["连铸作业区", "供辅作业区", "炼钢作业区"],
-			["3350轧钢作业区", "3800轧钢作业区"],
-			["外单位"]
-		];
-		var gf = document.getElementById("gasFactory");
-		var ga = document.getElementById("gasArea");
-		ga.length = 1;
-		var fl = gf.selectedIndex; //找到分厂位置，从而好使后面的区域与分厂对应
-		var fa = areas[fl - 1]; //先把所选分厂在数组中的位置找出来，确定是哪个分厂的几个区域
-		for(var j = 0; j < fa.length; j++) {
-			var op1 = document.createElement("option");
-			op1.innerHTML = fa[j];
-			ga.appendChild(op1);
-		}
-	},
-	/*getShop: function() {
-		var shops = [
-			[
-				["1#窑", "2#窑", "3#窑"],
-				["1#高炉", "2#高炉"],
-				["回转窑"]
-			],
-			[
-				["烤包"],
-				["脱硫", "煤气柜"],
-				["转炉"]
-			],
-			[
-				["1#加热炉", "5#加热炉"],
-				["均热坑", "加热炉"]
-			],
-			[
-				["成飞", "特材"]
-			],
-		];
-		var gf = document.getElementById("gasFactory");
-		var ga = document.getElementById("gasArea");
-		var gs = document.getElementById("gasShop")
-		gs.length = 1;
-		var fl = gf.selectedIndex;
-		var al = ga.selectedIndex;
-		var fa = shops[fl - 1];
-		var fas = fa[al - 1];
-		for(var k = 0; k < fas.length; k++) {
-			var op2 = document.createElement("option");
-			op2.innerHTML = fas[k];
-			gs.appendChild(op2);
-		}
-	},*/
 	chart: function() {
 		var instantChart = echarts.init($('#instantChart')[0]);
 		option = {
-		    title: {
-		        text: '气体用量折线图'
-		    },
-		    tooltip: {
-		        trigger: 'axis'
-		    },
-		    legend: {
-		        data:['煤气','氧气','氮气','氩气','压缩空气']
-		    },
-		    grid: {
-		        left: '3%',
-		        right: '4%',
-		        bottom: '3%',
-		        containLabel: true
-		    },
-		    toolbox: {
-		        feature: {
-		            saveAsImage: {}
-		        }
-		    },
-		    xAxis: {
-		        type: 'category',
-		        boundaryGap: false,
-		        data: ['周一','周二','周三','周四','周五','周六','周日']
-		    },
-		    yAxis: {
-		        type: 'value'
-		    },
-		    series: [
-		        {
-		            name:'煤气',
-		            type:'line',
-		            stack: '总量',
-		            data:[120, 132, 101, 134, 90, 230, 210]
-		        },
-		        {
-		            name:'氧气',
-		            type:'line',
-		            stack: '总量',
-		            data:[220, 182, 191, 234, 290, 330, 310]
-		        },
-		        {
-		            name:'氮气',
-		            type:'line',
-		            stack: '总量',
-		            data:[150, 232, 201, 154, 190, 330, 410]
-		        },
-		        {
-		            name:'氩气',
-		            type:'line',
-		            stack: '总量',
-		            data:[320, 332, 301, 334, 390, 330, 320]
-		        },{
-					name:'压缩空气',
-					type:'line',
+			title: {
+				text: '气体用量折线图'
+			},
+			tooltip: {
+				trigger: 'axis'
+			},
+			legend: {
+				data: ['煤气', '氧气', '氮气', '氩气', '压缩空气']
+			},
+			grid: {
+				left: '3%',
+				right: '4%',
+				bottom: '3%',
+				containLabel: true
+			},
+			toolbox: {
+				feature: {
+					saveAsImage: {}
+				}
+			},
+			xAxis: {
+				type: 'category',
+				boundaryGap: false,
+				data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+			},
+			yAxis: {
+				type: 'value'
+			},
+			series: [{
+					name: '煤气',
+					type: 'line',
 					stack: '总量',
-					data:[820, 932, 901, 934, 1290, 1330, 1320],
-					}]
+					data: [120, 132, 101, 134, 90, 230, 210]
+				},
+				{
+					name: '氧气',
+					type: 'line',
+					stack: '总量',
+					data: [220, 182, 191, 234, 290, 330, 310]
+				},
+				{
+					name: '氮气',
+					type: 'line',
+					stack: '总量',
+					data: [150, 232, 201, 154, 190, 330, 410]
+				},
+				{
+					name: '氩气',
+					type: 'line',
+					stack: '总量',
+					data: [320, 332, 301, 334, 390, 330, 320]
+				}, {
+					name: '压缩空气',
+					type: 'line',
+					stack: '总量',
+					data: [820, 932, 901, 934, 1290, 1330, 1320],
+				}
+			]
 		};
 		instantChart.setOption(option, true);
 	},
@@ -324,7 +257,7 @@ var GasReport = {
 			$(".ter-tab-item").eq(index).show().siblings().hide();
 			//$("#table").bootstrapTable("resetView");
 		});
-	
+
 		/* $(".tree-head-jz").on("click", function() {
 			var index = $(this).index();
 			$(this).addClass("tree-hleft").siblings().addClass("tree-hright");
@@ -332,7 +265,7 @@ var GasReport = {
 			$(".ztree").eq(index).show().siblings().hide();
 			//$("#table").bootstrapTable("resetView");
 		}); */
-	
+
 	}
 };
 $(function() {
